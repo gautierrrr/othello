cee422cd7f99f4e75f2716e897370d916b0b6a29
Keerthigan <corkla75012@gmail.com>
2017-04-14 00:23:57 +0200

    fonction qui permet de déterminer la victoire

--- othelloK/interface - Copie.cpp
+++ othelloK/interface - Copie.cpp
@@ -191,6 +191,87 @@ int affx = ma_case.x;
         m_cmptour = (m_cmptour+1)%2;      //on incrémente le compteur de tour
 }
 
+int Damier::verification(Coordonne ma_case)   ///fonction d"ajout d"un pion sur le plateau de jeu
+{
+    int affx = ma_case.x;
+    int affy = ma_case.y;
+    bool add = false;
+    int n=1;
+    vector<int> posx;
+    vector<int> posy;
+    vector<int> dirx;
+    vector<int> diry;
+    vector<int> liste_n;
+    int compt;
+
+
+    affx = ((affx/3)-(8/3));
+    affy = ((0.5*affy)-3);
+    string c =  (m_cmptour%2 == 0 ? "N" : "B");
+
+    for(int i=0; i<8; i++)
+    {
+        for(int j=0; j<8; j++)
+        {
+            for(int dx=-1; dx<=1; dx++)
+            {
+                for(int dy=-1; dy<=1; dy++)
+                {
+                    if (Map[i][j]->getType()!= " " && Map[i][j]->getType()!=c)
+                    {
+                        if(i+dx>=0&&i+dx<=7 && j+dy>=0&&j+dy<=7)
+                        {
+                            if(Map[i+dx][j+dy]->getType()==" ")
+                            {
+                                n = 1;
+                                while(i-dx*n>=0&&i-dx*n<=7 && j-dy*n>=0&&j-dy*n<=7)
+                                {
+                                    if(Map[i-dx*n][j-dy*n]->getType()==c)
+                                    {
+                                        posx.push_back(i+dx);
+                                        posy.push_back(j+dy);
+                                        dirx.push_back(dx);
+                                        diry.push_back(dy);
+                                        liste_n.push_back(n);
+                                        break;
+                                    }
+                                    else if(Map[i-dx*n][j-dy*n]->getType()==" ")
+                                        break;
+                                    else n++;
+                                }
+
+
+                            }
+
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+    cout << endl;
+    if(Map[affx][affy]->getType()==" " && !posx.empty())       //si la case est vide
+    {
+        for(int i=0; i<posx.size(); i++)
+        {
+            if(affx==posx[i]&&affy==posy[i])
+            {
+                add=true;
+
+                compt++;
+
+        }
+        }
+    }
+
+    if(add==true)
+        m_cmptour = (m_cmptour+1)%2;  //on incrémente le compteur de tour
+        return compt;
+
+
+}
+
 
 
 Coordonne Damier::curseur()
